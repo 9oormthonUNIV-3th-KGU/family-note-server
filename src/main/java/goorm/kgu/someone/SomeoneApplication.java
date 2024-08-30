c3162fa99f2e8452e519639bd7d5d238b511af1a
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-08-30 15:19:13 +0900

    initial commit

---
+++ src/main/java/goorm/kgu/someone/SomeoneApplication.java
@@ -0,0 +1,13 @@
+package goorm.kgu.someone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeoneApplication.class, args);
+	}
+
+}
