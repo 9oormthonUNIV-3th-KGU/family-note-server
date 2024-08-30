650c567fa7d07a922c5d1089cd40517b31ac93c8
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-08-30 18:15:27 +0900

    [GOORM-27]-feat: 커스텀 예외 처리

---
+++ src/main/java/goorm/kgu/someone/common/exception/ExceptionCode.java
@@ -0,0 +1,11 @@
+package goorm.kgu.someone.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionCode {
+	HttpStatus getStatus();
+
+	String getCode();
+
+	String getMessage();
+}
