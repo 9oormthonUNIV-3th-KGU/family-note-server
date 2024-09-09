619724c637a6050472af925e42629cd0d4918935
mingmingmon <mingmingmon@naver.com>
2024-09-09 22:19:42 +0900

    [GOORM-39]-feat: 기본 질문 생성 API 구현

---
+++ src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/domain/BaseQuestion.java
@@ -0,0 +1,39 @@
+package goorm.kgu.familynote.domain.question.baseQuestion.domain;
+
+import goorm.kgu.familynote.common.domain.BaseTimeEntity;
+import goorm.kgu.familynote.domain.family.family.domain.Family;
+import goorm.kgu.familynote.domain.family.member.domain.FamilyMember;
+import goorm.kgu.familynote.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "base_question")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseQuestion extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public static BaseQuestion create(String content) {
+        return BaseQuestion.builder()
+                .content(content)
+                .build();
+    }
+
+}
---
+++ src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/presentation/BaseQuestionController.java
@@ -0,0 +1,51 @@
+package goorm.kgu.familynote.domain.question.baseQuestion.presentation;
+
+import goorm.kgu.familynote.common.exception.ExceptionResponse;
+import goorm.kgu.familynote.domain.family.family.presentation.response.FamilyPersistResponse;
+import goorm.kgu.familynote.domain.family.member.presentation.request.FamilyMemberCreateRequest;
+import goorm.kgu.familynote.domain.question.baseQuestion.application.BaseQuestionService;
+import goorm.kgu.familynote.domain.question.baseQuestion.presentation.request.BaseQuestionCreateRequest;
+import goorm.kgu.familynote.domain.question.baseQuestion.presentation.response.BaseQuestionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/baseQuestion")
+@Tag(name = "Base Question", description = "기본 질문 관련 api / 담당자 : 전민주")
+public class BaseQuestionController {
+    private final BaseQuestionService baseQuestionService;
+
+    @Operation(summary = "기본 질문 등록", description = "기본 질문을 등록합니다. 개발자/관리자가 API를 통해서 조작할 수 있습니다.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "기본 질문 등록 성공",
+                    content = @Content(schema = @Schema(implementation = FamilyPersistResponse.class))
+            )
+    })
+    @ResponseStatus(CREATED)
+    @PostMapping
+    public ResponseEntity<List<BaseQuestionResponse>> createBaseQuestions(
+            @Valid @RequestBody List<BaseQuestionCreateRequest> requests
+    ) {
+        List<BaseQuestionResponse> response = baseQuestionService.saveBaseQuestions(requests);
+        return ResponseEntity.ok(response);
+    }
+
+}
