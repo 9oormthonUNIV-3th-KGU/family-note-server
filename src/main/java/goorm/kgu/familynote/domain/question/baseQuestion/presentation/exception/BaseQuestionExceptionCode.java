75acf736c1c4c1681e379f4342f658a7258be266
mingmingmon <mingmingmon@naver.com>
2024-09-12 23:07:13 +0900

    [GOORM-42]-fix: 상태코드 오름차순으로 ExceptionCode 정렬

--- src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/presentation/exception/BaseQuestionExceptionCode.java
+++ src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/presentation/exception/BaseQuestionExceptionCode.java
@@ -13,8 +13,8 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 @AllArgsConstructor
 public enum BaseQuestionExceptionCode implements ExceptionCode {
     BASE_QUESTION_NOT_FOUND(NOT_FOUND, "해당 기본 질문을 찾을 수 없습니다."),
-    INVALID_BASE_QUESTION_COUNT(UNPROCESSABLE_ENTITY, "기본 질문이 0개 이하입니다."),
     NO_MORE_BASE_QUESTION_EXCEPTION(CONFLICT, "더 이상 제공할 기본 질문이 없습니다."),
+    INVALID_BASE_QUESTION_COUNT(UNPROCESSABLE_ENTITY, "기본 질문이 0개 이하입니다."),
     ;
 
     private final HttpStatus status;
