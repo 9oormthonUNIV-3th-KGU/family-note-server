e80f0d19d775f1cfe922516478de30ca76493104
mingmingmon <mingmingmon@naver.com>
2024-09-14 10:50:08 +0900

    [GOORM-42]-fix: familyQuestionPageResponse 응답 example 추가

--- src/main/java/goorm/kgu/familynote/domain/family/familyQuestion/presentation/response/FamilyQuestionPageResponse.java
+++ src/main/java/goorm/kgu/familynote/domain/family/familyQuestion/presentation/response/FamilyQuestionPageResponse.java
@@ -7,16 +7,17 @@ import java.util.List;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public record FamilyQuestionPageResponse<T>(
-        @Schema(
-                description = "가족 질문 리스트",
-                requiredMode = REQUIRED
-        )
+        @Schema(description = "가족 질문 리스트",
+                example = "[\n{\n\"familyQuestionId\": 196,\n" +
+                        " \"content\": \"오늘 친구들과 있었던 재미있는 일이나 고민이 있었나요?\",\n" +
+                        " \"createdAt\": \"2024-09-13T21:48:52.450273\"\n},\n" +
+                        " {\n\"familyQuestionId\": 195,\n" +
+                        "  \"content\": \"최근에 읽은 책이나 본 영상 중 추천하고 싶은 것이 있나요?\",\n" +
+                        "  \"createdAt\": \"2024-09-13T21:48:34.456825\"\n}\n]",
+                requiredMode = REQUIRED)
         List<FamilyQuestionResponse> contents,
 
-        @Schema(
-                description = "페이징 정보",
-                requiredMode = REQUIRED
-        )
+        @Schema(description = "페이징 정보", requiredMode = REQUIRED)
         PageableResponse<T> pageable
 ) {
 
