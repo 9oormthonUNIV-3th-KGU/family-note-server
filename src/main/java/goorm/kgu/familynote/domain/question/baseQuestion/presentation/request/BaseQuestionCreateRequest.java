6e1eb80eab0f01750efd7da685a0a1e21e0de818
mingmingmon <96719969+mingmingmon@users.noreply.github.com>
2024-09-15 22:37:37 +0900

    [GOORM-67]-feat: 기본 질문 길이 제한 설정 (#11)

--- src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/presentation/request/BaseQuestionCreateRequest.java
+++ src/main/java/goorm/kgu/familynote/domain/question/baseQuestion/presentation/request/BaseQuestionCreateRequest.java
@@ -1,11 +1,13 @@
 package goorm.kgu.familynote.domain.question.baseQuestion.presentation.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public record BaseQuestionCreateRequest(
         @Schema(description = "기본 질문 내용", example = "탕수육 먹을 때 부먹 vs 찍먹", requiredMode = REQUIRED)
+        @Size(min = 1, max = 50)
         String content
 ) {
 }
